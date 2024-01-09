1fbd9566828582e778d8ad7aedee0fbf0c0ba2fd
Furkan <100231836+aselichi@users.noreply.github.com>
2024-01-09 10:58:14 +0300

    login fix

--- frontend/hotel_management_system/src/components/User/Login.jsx
+++ frontend/hotel_management_system/src/components/User/Login.jsx
@@ -58,6 +58,7 @@ const Login = () => {
       <Box
         sx={{
           marginTop: 8,
+          marginBottom:20,
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
